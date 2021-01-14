256108f22f96a56adaae5fbc22f3b0251bed2a38
Eduard <noizighenya@gmail.com>
2021-01-14 18:58:37 +0200

    create layout

---
+++ add.js
@@ -0,0 +1,24 @@
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fname) => {
+  
+  
+  
+  console.log (`Hello linting, ${fName}`);
+
+
+
+  
+};
+
+
+sayHelloLinting ("James");
+
+
+
+
+
